a104a1bc496ee68aa8ae7d8dd9bc6594268421a4
Ozan <ozancangu24@gmail.com>
2022-12-07 00:12:39 +0300

    -Di part HiltApplication.kt created

---
+++ app/src/main/java/com/ozancanguz/mvvmrickandmortyapp/HiltApplication.kt
@@ -0,0 +1,8 @@
+package com.ozancanguz.mvvmrickandmortyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication:Application() {
+}
--- app/src/main/java/com/ozancanguz/mvvmrickandmortyapp/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/mvvmrickandmortyapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ozancanguz.mvvmrickandmortyapp
+package com.ozancanguz.mvvmrickandmortyapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ozancanguz.mvvmrickandmortyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
